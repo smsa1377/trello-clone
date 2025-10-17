6d4ab38da61a111b2b1f36be2d9cf00181433a87
smsa1377 <smsa1377@gmail.com>
2025-10-18 00:07:23 +0330

    clean unused css in ButtonCnb.module.scss

--- src/components/ui/ButtonCnb.tsx
+++ src/components/ui/ButtonCnb.tsx
@@ -25,9 +25,8 @@ export default function ButtonCnb() {
                         <input name="board-name" type="text" placeholder="Add board title" className={styles.input_cnb}
                                onChange={handleButtonCNB}/>
                         <div className={styles.checkbox_color}>
-                            {[1, 2, 3, 4, 5].map((i, n) =>
-                                <div className={`${styles.checkbox_color_item}`}
-                                     key={n}>
+                            {[1, 2, 3, 4, 5].map((i: number, n: number) =>
+                                <div className={`${styles.checkbox_color_item} ${i}`} key={n}>
                                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
                                          className="svg-inline--fa fa-check fa-w-16 fa-null fa-rotate-null fa-pull-null "
                                          role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
